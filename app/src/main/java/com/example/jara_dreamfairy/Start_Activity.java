ab27e8ef6c3aeb3ca86677665680495b10fc5126
Bing-su <57391343+Bingsu-kun@users.noreply.github.com>
2020-11-08 00:23:27 +0900

    images update

--- app/src/main/java/com/example/jara_dreamfairy/Start_Activity.java
+++ app/src/main/java/com/example/jara_dreamfairy/Start_Activity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
 import android.view.Window;
+import android.view.WindowManager;
 import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,7 +20,7 @@ public class Start_Activity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_start);
 
         firebaseAuth = FirebaseAuth.getInstance();
